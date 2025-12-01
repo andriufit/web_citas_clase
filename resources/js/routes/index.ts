d91cfb581553a887f10f109d0c39ebeed5840fa3
Andrés Vargas <34950192+andriufit@users.noreply.github.com>
2025-12-01 16:30:17 +0100

    Add front-end route helper definitions

---
+++ resources/js/routes/index.ts
@@ -0,0 +1,7 @@
+import { createRoute } from './helpers';
+
+export const home = () => createRoute('/');
+export const dashboard = () => createRoute('/dashboard');
+export const login = () => createRoute('/login');
+export const register = () => createRoute('/register');
+export const logout = () => createRoute('/logout', 'post');
